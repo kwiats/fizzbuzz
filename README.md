42b715e911f972d978790e8b1e17210e66e5ecba
kwiats <pawuskwiatkowski@gmail.com>
2022-09-01 15:52:00 +0200

    Added new features

---
+++ README.md
@@ -0,0 +1,19 @@
+# FizzBuzz
+A simple game to test the skills of a new future programmer while conducting an interview.
+
+### Installing
+
+You should make clone of repository on your local machine. You can use this command in your terminal:
+
+    $git clone https://github.com/kwiats/fizzbuzz fizzbuzz
+
+Then,
+
+    $python main.py 
+
+If you want to change range of number, you can do it. Use this command:
+
+    $python main.py <value1 :int> <value2 :int>
+
+
+### Enjoy! <3
--- main.py
+++ main.py
@@ -1,8 +1,33 @@
-for i in range(1, 101):
-    if (i%15 == 0):
-        print(f"{i} - FizzBuzz")
-    elif (i%3 == 0 ):
-        print(f"{i} - Fizz")
-    elif (i%5 == 0):
-        print(f"{i} - Buzz")
-    
+import sys
+
+list_of_fizzbuzz = []
+list_of_fizz = []
+list_of_buzz = []
+
+
+def fizzbuzz(range_from: int, range_to: int) -> dict:
+    for i in range(range_from, range_to):
+        if (i % 15 == 0):
+            list_of_fizzbuzz.append(i)
+        elif (i % 3 == 0):
+            list_of_fizz.append(i)
+        elif (i % 5 == 0):
+            list_of_buzz.append(i)
+
+    return {'fizzbuzz': list_of_fizzbuzz,
+            'fizz': list_of_fizz,
+            'buzz': list_of_buzz}
+
+
+def show_results(func: dict):
+    for title, numbers in func.items():
+        print("\n" + title.upper())
+        for number in numbers:
+            print(number, end=" ")
+
+
+if __name__ == "__main__":
+    range_from = int(sys.argv[0])
+    range_to = int(sys.argv[1])
+    show_results(fizzbuzz(range_from,
+                          range_to))
